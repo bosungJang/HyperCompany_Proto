7cda46afabc73bdf670c5e54e4526536c782b0b9
bosungJang <bosung_jang@tmax.co.kr>
2022-07-20 10:04:48 +0900

    개발 상세 페이지 추가

--- react-micro/src/components/Fi/FinancialStatementSettingDetail.tsx
+++ react-micro/src/components/Fi/FinancialStatementSettingDetail.tsx
@@ -7,6 +7,8 @@ import queryString from "query-string";
 import HcRadioGroup, { HcRadioButton } from "common/HcRadioButton";
 import HcButton from "common/HcButton";
 import { FilterButton, SettingButton } from "common/HcTableButton";
+import HcBottomBar from "common/HcBottomBar";
+import "common/Table.css";
 
 const SolidWrapper = styled.div`
   border: 1px solid #cecece;
@@ -104,243 +106,275 @@ const FinancailStatementSettingDetail = ({
   const query = queryString.parse(location.search);
 
   const [detailData, setDetailData] = React.useState(data);
+  const [barOpen, setbarOpen] = React.useState(true);
+
+  const history = useHistory();
 
   return (
-    <div style={{ display: "block", width: "100%", marginTop: "16px" }}>
-      <HcTitleTextField
-        titleName="재무제표 양식 상세"
-        isBackIcon={true}
-        style={{ display: "inline-block" }}
-      />
-      <SolidWrapper>
-        <VariableMultiLayout>
-          <p
-            style={{
-              flexGrow: 1,
-              marginBlockStart: "0px",
-              marginBlockEnd: "0px",
-            }}
-          >
-            <Wrapper>
-              <Title required={false}>{"재무제표코드"}</Title>
-              <LabelPictureTextField>{query.id}</LabelPictureTextField>
-            </Wrapper>
-            <Wrapper style={{ marginTop: "20px" }}>
-              <Title required={false}>{"적용기간"}</Title>
-              <LabelPictureTextField>
-                {detailData.startDate}~{detailData.endDate}
-              </LabelPictureTextField>
-            </Wrapper>
-          </p>
-          <p
-            style={{
-              flexGrow: 1,
-              marginBlockStart: "0px",
-              marginBlockEnd: "0px",
-            }}
-          >
-            <Wrapper>
-              <Title required={false}>{"재무제표명"}</Title>
-              <LabelPictureTextField>
-                {detailData.stateName}
-              </LabelPictureTextField>
-            </Wrapper>
-            <Wrapper style={{ marginTop: "20px" }}>
-              <Title>{"대체 전표"}</Title>
-              <div style={{ marginTop: "20px" }}>
-                <HcRadioGroup
-                  defaultValue={detailData ? "use" : "usenot"}
-                  onChange={(value) => {
-                    /*
+    <>
+      <div style={{ display: "block", width: "100%", marginTop: "16px" }}>
+        <HcTitleTextField
+          titleName="재무제표 양식 상세"
+          isBackIcon={true}
+          style={{ display: "inline-block" }}
+        />
+        <SolidWrapper>
+          <VariableMultiLayout>
+            <p
+              style={{
+                flexGrow: 1,
+                marginBlockStart: "0px",
+                marginBlockEnd: "0px",
+              }}
+            >
+              <Wrapper>
+                <Title required={false}>{"재무제표코드"}</Title>
+                <LabelPictureTextField>{query.id}</LabelPictureTextField>
+              </Wrapper>
+              <Wrapper style={{ marginTop: "20px" }}>
+                <Title required={false}>{"적용기간"}</Title>
+                <LabelPictureTextField>
+                  {detailData.startDate}~{detailData.endDate}
+                </LabelPictureTextField>
+              </Wrapper>
+            </p>
+            <p
+              style={{
+                flexGrow: 1,
+                marginBlockStart: "0px",
+                marginBlockEnd: "0px",
+              }}
+            >
+              <Wrapper>
+                <Title required={false}>{"재무제표명"}</Title>
+                <LabelPictureTextField>
+                  {detailData.stateName}
+                </LabelPictureTextField>
+              </Wrapper>
+              <Wrapper style={{ marginTop: "20px" }}>
+                <Title>{"대체 전표"}</Title>
+                <div style={{ marginTop: "20px" }}>
+                  <HcRadioGroup
+                    defaultValue={detailData ? "use" : "usenot"}
+                    onChange={(value) => {
+                      /*
                     setcreateData((prevState) => ({
                       ...prevState,
                       enable: value,
                     }));
                     console.log(createData.enable);
                     */
-                  }}
-                >
-                  <HcRadioButton value="use" disabled>
-                    <span
-                      style={{
-                        marginLeft: "8px",
-                        fontFamily: "Noto Sans KR",
-                        fontSize: "14px",
-                        marginRight: "60px",
-                      }}
-                    >
-                      사용
-                    </span>
-                  </HcRadioButton>
-                  <HcRadioButton value="usenot" disabled>
-                    <span
-                      style={{
-                        marginLeft: "8px",
-                        fontFamily: "Noto Sans KR",
-                        fontSize: "14px",
-                      }}
-                    >
-                      사용 안함
-                    </span>
-                  </HcRadioButton>
-                </HcRadioGroup>
-              </div>
-            </Wrapper>
-          </p>
-          <p
+                    }}
+                  >
+                    <HcRadioButton value="use" disabled>
+                      <span
+                        style={{
+                          marginLeft: "8px",
+                          fontFamily: "Noto Sans KR",
+                          fontSize: "14px",
+                          marginRight: "60px",
+                        }}
+                      >
+                        사용
+                      </span>
+                    </HcRadioButton>
+                    <HcRadioButton value="usenot" disabled>
+                      <span
+                        style={{
+                          marginLeft: "8px",
+                          fontFamily: "Noto Sans KR",
+                          fontSize: "14px",
+                        }}
+                      >
+                        사용 안함
+                      </span>
+                    </HcRadioButton>
+                  </HcRadioGroup>
+                </div>
+              </Wrapper>
+            </p>
+            <p
+              style={{
+                flexGrow: 1,
+                marginBlockStart: "0px",
+                marginBlockEnd: "0px",
+              }}
+            >
+              <Wrapper>
+                <Title required={false}>{"양식구분"}</Title>
+                <LabelPictureTextField>
+                  {detailData.formClassification}
+                </LabelPictureTextField>
+              </Wrapper>
+              <Wrapper style={{ marginTop: "20px" }}>
+                <Title required={false}>{"올림구분"}</Title>
+                <LabelPictureTextField>
+                  {detailData.round}
+                </LabelPictureTextField>
+              </Wrapper>
+            </p>
+          </VariableMultiLayout>
+        </SolidWrapper>
+        <div style={{ width: "100%", minHeight: "600px", marginTop: "38px" }}>
+          <div
             style={{
-              flexGrow: 1,
-              marginBlockStart: "0px",
-              marginBlockEnd: "0px",
+              width: "658px",
+              display: "inline-block",
+              borderRight: "1px solid #CECECE",
+              paddingRight: "12px",
             }}
           >
-            <Wrapper>
-              <Title required={false}>{"양식구분"}</Title>
-              <LabelPictureTextField>
-                {detailData.formClassification}
-              </LabelPictureTextField>
-            </Wrapper>
-            <Wrapper style={{ marginTop: "20px" }}>
-              <Title required={false}>{"올림구분"}</Title>
-              <LabelPictureTextField>{detailData.round}</LabelPictureTextField>
-            </Wrapper>
-          </p>
-        </VariableMultiLayout>
-      </SolidWrapper>
-      <div style={{ width: "100%", minHeight: "600px", marginTop: "38px" }}>
-        <div
-          style={{
-            width: "658px",
-            display: "inline-block",
-            borderRight: "1px solid #CECECE",
-            paddingRight: "12px",
-          }}
-        >
-          <TableTitle>계정그룹</TableTitle>
-          <div style={{ marginTop: "18px" }}>
-            <HcButton styles="secondary" size="medium">
-              생성
-            </HcButton>
-            <div style={{ float: "right", display: "flex" }}>
-              <FilterButton style={{ marginRight: "6px" }} />
-              <SettingButton />
-            </div>
-            <TableContainer>
-              <table className="table table-hover" style={{ width: "100%" }}>
-                <thead
-                  style={{
-                    display: "table",
-                    width: "inherit",
-                    tableLayout: "fixed",
-                  }}
-                >
-                  <tr>
-                    <th>계정코드</th>
-                    <th>계정명</th>
-                    <th>계정레벨</th>
-                    <th>상위계정</th>
-                    <th>차/대구분</th>
-                    <th>그룹속성</th>
-                    <th>사용여부</th>
-                    <th>출력순서</th>
-                  </tr>
-                </thead>
-                <tbody
-                  style={{
-                    display: "block",
-                    overflow: "overlay",
-                    maxHeight: 446,
-                  }}
-                >
-                  {accountGroupData.map((item) => (
-                    <tr
-                      style={{
-                        display: "table",
-                        width: "100%",
-                        tableLayout: "fixed",
-                        height: "46px",
-                      }}
-                    >
-                      <td>{item.code}</td>
-                      <td>{item.name}</td>
-                      <td>{item.level}</td>
-                      <td>{item.parentAccount}</td>
-                      <td>{item.debitCredit}</td>
-                      <td>{item.group}</td>
-                      <td>{item.useYn ? "사용" : "사용안함"}</td>
-                      <td>{item.outputOrder}</td>
+            <TableTitle>계정그룹</TableTitle>
+            <div style={{ marginTop: "18px" }}>
+              <HcButton styles="secondary" size="medium">
+                생성
+              </HcButton>
+              <div style={{ float: "right", display: "flex" }}>
+                <FilterButton style={{ marginRight: "6px" }} />
+                <SettingButton />
+              </div>
+              <TableContainer>
+                <table className="table table-hover" style={{ width: "100%" }}>
+                  <thead
+                    style={{
+                      display: "table",
+                      width: "inherit",
+                      tableLayout: "fixed",
+                    }}
+                  >
+                    <tr>
+                      <th>계정코드</th>
+                      <th>계정명</th>
+                      <th>계정레벨</th>
+                      <th>상위계정</th>
+                      <th>차/대구분</th>
+                      <th>그룹속성</th>
+                      <th>사용여부</th>
+                      <th>출력순서</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </TableContainer>
+                  </thead>
+                  <tbody
+                    style={{
+                      display: "block",
+                      overflow: "overlay",
+                      maxHeight: 446,
+                    }}
+                  >
+                    {accountGroupData.map((item) => (
+                      <tr
+                        style={{
+                          display: "table",
+                          width: "100%",
+                          tableLayout: "fixed",
+                          height: "46px",
+                        }}
+                      >
+                        <td>{item.code}</td>
+                        <td>{item.name}</td>
+                        <td>{item.level}</td>
+                        <td>{item.parentAccount}</td>
+                        <td>{item.debitCredit}</td>
+                        <td>{item.group}</td>
+                        <td>{item.useYn ? "사용" : "사용안함"}</td>
+                        <td>{item.outputOrder}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </TableContainer>
+            </div>
+          </div>
+          <div
+            style={{
+              width: "658px",
+              display: "inline-block",
+              paddingLeft: "11px",
+            }}
+          >
+            <TableTitle>계정과목</TableTitle>
+            <div style={{ marginTop: "18px" }}>
+              <HcButton styles="secondary" size="medium">
+                생성
+              </HcButton>
+              <div style={{ float: "right", display: "flex" }}>
+                <FilterButton style={{ marginRight: "6px" }} />
+                <SettingButton />
+              </div>
+              <TableContainer>
+                <table className="table table-hover" style={{ width: "100%" }}>
+                  <thead
+                    style={{
+                      display: "table",
+                      width: "inherit",
+                      tableLayout: "fixed",
+                    }}
+                  >
+                    <tr>
+                      <th>계정코드</th>
+                      <th>계정명</th>
+                      <th>출력계정명</th>
+                      <th>상위계정</th>
+                      <th>사용여부</th>
+                      <th>출력순서</th>
+                    </tr>
+                  </thead>
+                  <tbody
+                    style={{
+                      display: "block",
+                      overflow: "overlay",
+                      maxHeight: 446,
+                    }}
+                  >
+                    {accountSubjectData.map((item) => (
+                      <tr
+                        style={{
+                          display: "table",
+                          width: "100%",
+                          tableLayout: "fixed",
+                          height: "46px",
+                        }}
+                      >
+                        <td>{item.code}</td>
+                        <td>{item.name}</td>
+                        <td>{item.outputName}</td>
+                        <td>{item.parentAccount}</td>
+                        <td>{item.useYn ? "사용" : "사용안함"}</td>
+                        <td>{item.outputOrder}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </TableContainer>
+            </div>
           </div>
         </div>
-        <div
-          style={{
-            width: "658px",
-            display: "inline-block",
-            paddingLeft: "11px",
-          }}
-        >
-          <TableTitle>계정과목</TableTitle>
-          <div style={{ marginTop: "18px" }}>
-            <HcButton styles="secondary" size="medium">
-              생성
+        <HcBottomBar open={barOpen}>
+          <div>
+            <HcButton
+              onClick={() => {
+                //openModal();
+              }}
+              styles="primary"
+              style={{ marginRight: "5px" }}
+              size="big"
+            >
+              승인
             </HcButton>
-            <div style={{ float: "right", display: "flex" }}>
-              <FilterButton style={{ marginRight: "6px" }} />
-              <SettingButton />
-            </div>
-            <TableContainer>
-              <table className="table table-hover" style={{ width: "100%" }}>
-                <thead
-                  style={{
-                    display: "table",
-                    width: "inherit",
-                    tableLayout: "fixed",
-                  }}
-                >
-                  <tr>
-                    <th>계정코드</th>
-                    <th>계정명</th>
-                    <th>출력계정명</th>
-                    <th>상위계정</th>
-                    <th>사용여부</th>
-                    <th>출력순서</th>
-                  </tr>
-                </thead>
-                <tbody
-                  style={{
-                    display: "block",
-                    overflow: "overlay",
-                    maxHeight: 446,
-                  }}
-                >
-                  {accountSubjectData.map((item) => (
-                    <tr
-                      style={{
-                        display: "table",
-                        width: "100%",
-                        tableLayout: "fixed",
-                        height: "46px",
-                      }}
-                    >
-                      <td>{item.code}</td>
-                      <td>{item.name}</td>
-                      <td>{item.outputName}</td>
-                      <td>{item.parentAccount}</td>
-                      <td>{item.useYn ? "사용" : "사용안함"}</td>
-                      <td>{item.outputOrder}</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </TableContainer>
+
+            <HcButton
+              onClick={() => {
+                setbarOpen(false);
+              }}
+              styles="line"
+              style={{ marginRight: "5px" }}
+              size="big"
+            >
+              반려
+            </HcButton>
           </div>
-        </div>
+        </HcBottomBar>
       </div>
-    </div>
+    </>
   );
 };
 
